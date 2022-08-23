888444d0a48e1f7c1083d5be3207250105b3fc90
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-08-23 09:03:10 +0600

    added files

---
+++ js/calculation.js
@@ -0,0 +1,44 @@
+function getInputFieldById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldString = inputField.value;
+    const inputValue = parseFloat(inputFieldString);
+    inputField.value = '';
+    return inputValue;
+}
+
+function getTextFieldById(textFieldId) {
+    const textField = document.getElementById(textFieldId);
+    const textFieldString = textField.innerText;
+    const textValue = parseFloat(textFieldString);
+    return textValue;
+}
+
+function setTextElementValueById(textFieldId, newValue) {
+    const textField = document.getElementById(textFieldId);
+    textField.innerText = newValue;
+}
+
+
+
+
+
+document.getElementById('btn-player').addEventListener('click', function () {
+    const perPlayerCost = getInputFieldById('player-cost');
+    const fivePlayerCost = perPlayerCost * 5;
+    const totalPlayer = getTextFieldById('total-playercost');
+    const totalPlayerCost = totalPlayer + fivePlayerCost;
+
+    setTextElementValueById('total-playercost', totalPlayerCost);
+})
+
+document.getElementById('btn-total').addEventListener('click', function () {
+    const totalPlayer = getTextFieldById('total-playercost');
+    const managerCost = getInputFieldById('manager-cost');
+    const coachCost = getInputFieldById('caoch-cost');
+    const totalCost = getTextFieldById('total-cost');
+    const totalAmount = managerCost + coachCost + totalCost + totalPlayer;
+    setTextElementValueById('total-cost', totalAmount);
+
+})
+
+
---
+++ js/cart.js
@@ -0,0 +1,47 @@
+const cartArray = [];
+
+function displayPlayers(players) {
+
+    const playerList = document.getElementById('players-cart');
+    playerList.innerHTML = '';
+
+
+    if (cartArray.length < 6) {
+
+        for (let i = 0; i < players.length; i++) {
+            const addPlayer = cartArray[i].playerName;
+
+
+
+            const ol = document.createElement('ol');
+            ol.innerHTML = `
+            <li>${i + 1}. <span>${addPlayer}</span></li>
+            `;
+            playerList.appendChild(ol);
+        }
+
+    }
+
+    else {
+        alert('You have selected Top Five!')
+    }
+
+}
+
+
+
+function addToCart(element) {
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+
+    const playerObj = {
+        playerName: playerName,
+    }
+    cartArray.push(playerObj);
+
+    document.getElementById('player-count').innerText = cartArray.length;
+    displayPlayers(cartArray);
+
+
+
+}
+
